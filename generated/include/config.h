28fec311f4c096835f957886093ecd7e5a98e129
Charalampos Mainas <Charalampos.Mainas@neclab.eu>
2019-09-26 17:31:29 +0200

    Add Makefile.uk
    This patch also adds the generated config.h and runtime.inc headers.
    
    Signed-off-by: Charalampos Mainas <Charalampos.Mainas@neclab.eu>
    Signed-off-by: Costin Lupu <costin.lupu@cs.pub.ro>
    Reviewed-by: Felipe Huici <felipe.huici@neclab.eu>

---
+++ generated/include/config.h
@@ -0,0 +1,415 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define if building universal (internal helper macro) */
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
+
+/* Define to the flags needed for the .section .eh_frame directive. */
+#define EH_FRAME_FLAGS "aw"
+
+/* Define to 1 if you have the `accept4' function. */
+//#define HAVE_ACCEPT4 1
+
+/* Define to 1 if you have the `acosl' function. */
+#define HAVE_ACOSL 1
+
+/* Define to 1 if you have the `asinl' function. */
+#define HAVE_ASINL 1
+
+/* Define if your assembler supports GNU comdat group syntax. */
+#define HAVE_AS_COMDAT_GAS 1
+
+/* Define if your assembler supports unwind section type. */
+#define HAVE_AS_X86_64_UNWIND_SECTION_TYPE 1
+
+/* Define if your assembler supports AES instructions. */
+//#define HAVE_AS_X86_AES 1
+
+/* Define if your assembler supports PC relative relocs. */
+#define HAVE_AS_X86_PCREL 1
+
+/* Define to 1 if you have the `atan2l' function. */
+#define HAVE_ATAN2L 1
+
+/* Define to 1 if you have the `atanl' function. */
+#define HAVE_ATANL 1
+
+/* Define to 1 if you have the `cosl' function. */
+#define HAVE_COSL 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the `dl_iterate_phdr' function. */
+#define HAVE_DL_ITERATE_PHDR 1
+
+/* Define to 1 if you have the `dup3' function. */
+#define HAVE_DUP3 1
+
+/* Define to 1 if you have the `epoll_create1' function. */
+//#define HAVE_EPOLL_CREATE1 1
+
+/* Define to 1 if you have the `expl' function. */
+#define HAVE_EXPL 1
+
+/* Define to 1 if you have the `expm1l' function. */
+#define HAVE_EXPM1L 1
+
+/* Define to 1 if you have the `faccessat' function. */
+#define HAVE_FACCESSAT 1
+
+/* Define to 1 if you have the `fallocate' function. */
+#define HAVE_FALLOCATE 1
+
+/* Define to 1 if you have the `fchmodat' function. */
+#define HAVE_FCHMODAT 1
+
+/* Define to 1 if you have the `fchownat' function. */
+//#define HAVE_FCHOWNAT 1
+
+/* Define to 1 if you have the `futimesat' function. */
+//#define HAVE_FUTIMESAT 1
+
+/* Define if _Unwind_GetIPInfo is available. */
+#define HAVE_GETIPINFO 1
+
+/* Define to 1 if you have the `getxattr' function. */
+//#define HAVE_GETXATTR 1
+
+/* Define to 1 if you have the `inotify_add_watch' function. */
+#define HAVE_INOTIFY_ADD_WATCH 1
+
+/* Define to 1 if you have the `inotify_init' function. */
+#define HAVE_INOTIFY_INIT 1
+
+/* Define to 1 if you have the `inotify_init1' function. */
+#define HAVE_INOTIFY_INIT1 1
+
+/* Define to 1 if you have the `inotify_rm_watch' function. */
+#define HAVE_INOTIFY_RM_WATCH 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `ldexpl' function. */
+#define HAVE_LDEXPL 1
+
+/* Define to 1 if you have the <linux/ether.h> header file. */
+/* #undef HAVE_LINUX_ETHER_H */
+
+/* Define to 1 if you have the <linux/filter.h> header file. */
+//#define HAVE_LINUX_FILTER_H 1
+
+/* Define to 1 if you have the <linux/fs.h> header file. */
+//#define HAVE_LINUX_FS_H 1
+
+/* Define to 1 if you have the <linux/if_addr.h> header file. */
+//#define HAVE_LINUX_IF_ADDR_H 1
+
+/* Define to 1 if you have the <linux/if_ether.h> header file. */
+//#define HAVE_LINUX_IF_ETHER_H 1
+
+/* Define to 1 if you have the <linux/if_tun.h> header file. */
+//#define HAVE_LINUX_IF_TUN_H 1
+
+/* Define to 1 if you have the <linux/netlink.h> header file. */
+//#define HAVE_LINUX_NETLINK_H 1
+
+/* Define to 1 if you have the <linux/ptrace.h> header file. */
+//#define HAVE_LINUX_PTRACE_H 1
+
+/* Define to 1 if you have the <linux/reboot.h> header file. */
+//#define HAVE_LINUX_REBOOT_H 1
+
+/* Define to 1 if you have the <linux/rtnetlink.h> header file. */
+//#define HAVE_LINUX_RTNETLINK_H 1
+
+/* Define to 1 if you have the `listxattr' function. */
+//#define HAVE_LISTXATTR 1
+
+/* Define to 1 if the system has the type `loff_t'. */
+#define HAVE_LOFF_T 1
+
+/* Define to 1 if you have the `log10l' function. */
+#define HAVE_LOG10L 1
+
+/* Define to 1 if you have the `log1pl' function. */
+#define HAVE_LOG1PL 1
+
+/* Define to 1 if you have the `logl' function. */
+#define HAVE_LOGL 1
+
+/* Define to 1 if you have the `matherr' function. */
+/* #undef HAVE_MATHERR */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `mincore' function. */
+//#define HAVE_MINCORE 1
+
+/* Define to 1 if you have the `mkdirat' function. */
+//#define HAVE_MKDIRAT 1
+
+/* Define to 1 if you have the `mknodat' function. */
+//#define HAVE_MKNODAT 1
+
+/* Define to 1 if you have the <netinet/icmp6.h> header file. */
+//#define HAVE_NETINET_ICMP6_H 1
+
+/* Define to 1 if you have the <netinet/if_ether.h> header file. */
+//#define HAVE_NETINET_IF_ETHER_H 1
+
+/* Define to 1 if you have the <netinet/in_syst.h> header file. */
+/* #undef HAVE_NETINET_IN_SYST_H */
+
+/* Define to 1 if you have the <netinet/ip.h> header file. */
+//#define HAVE_NETINET_IP_H 1
+
+/* Define to 1 if you have the <netinet/ip_mroute.h> header file. */
+/* #undef HAVE_NETINET_IP_MROUTE_H */
+
+/* Define to 1 if you have the <netpacket/packet.h> header file. */
+//#define HAVE_NETPACKET_PACKET_H 1
+
+/* Define to 1 if you have the <net/if_arp.h> header file. */
+//#define HAVE_NET_IF_ARP_H 1
+
+/* Define to 1 if you have the <net/if.h> header file. */
+//#define HAVE_NET_IF_H 1
+
+/* Define to 1 if you have the <net/route.h> header file. */
+//#define HAVE_NET_ROUTE_H 1
+
+/* Define to 1 if the system has the type `off64_t'. */
+#define HAVE_OFF64_T 1
+
+/* Define to 1 if you have the `open64' function. */
+#define HAVE_OPEN64 1
+
+/* Define to 1 if you have the `openat' function. */
+#define HAVE_OPENAT 1
+
+/* Define to 1 if you have the `pipe2' function. */
+//#define HAVE_PIPE2 1
+
+/* Define to 1 if you have the <port.h> header file. */
+/* #undef HAVE_PORT_H */
+
+/* Define to 1 if you have the `removexattr' function. */
+//#define HAVE_REMOVEXATTR 1
+
+/* Define to 1 if you have the `renameat' function. */
+//#define HAVE_RENAMEAT 1
+
+/* Define to 1 if you have the <sched.h> header file. */
+#define HAVE_SCHED_H 1
+
+/* Define to 1 if you have the <semaphore.h> header file. */
+#define HAVE_SEMAPHORE_H 1
+
+/* Define to 1 if you have the `sem_timedwait' function. */
+#define HAVE_SEM_TIMEDWAIT 1
+
+/* Define to 1 if you have the `setenv' function. */
+#define HAVE_SETENV 1
+
+/* Define to 1 if you have the `setxattr' function. */
+#define HAVE_SETXATTR 1
+
+/* Define to 1 if you have the `sinl' function. */
+#define HAVE_SINL 1
+
+/* Define to 1 if you have the `splice' function. */
+//#define HAVE_SPLICE 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `strerror_r' function. */
+#define HAVE_STRERROR_R 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strsignal' function. */
+#define HAVE_STRSIGNAL 1
+
+/* Define to 1 if <math.h> defines struct exception */
+/* #undef HAVE_STRUCT_EXCEPTION */
+
+/* Define to 1 if the compiler provides the __sync_add_and_fetch function for
+   uint64 */
+#define HAVE_SYNC_ADD_AND_FETCH_8 1
+
+/* Define to 1 if the compiler provides the __sync_bool_compare_and_swap
+   function for uint32 */
+#define HAVE_SYNC_BOOL_COMPARE_AND_SWAP_4 1
+
+/* Define to 1 if the compiler provides the __sync_bool_compare_and_swap
+   function for uint64 */
+#define HAVE_SYNC_BOOL_COMPARE_AND_SWAP_8 1
+
+/* Define to 1 if the compiler provides the __sync_fetch_and_add function for
+   uint32 */
+#define HAVE_SYNC_FETCH_AND_ADD_4 1
+
+/* Define to 1 if you have the `sync_file_range' function. */
+#define HAVE_SYNC_FILE_RANGE 1
+
+/* Define to 1 if you have the <syscall.h> header file. */
+#define HAVE_SYSCALL_H 1
+
+/* Define to 1 if you have the <sys/epoll.h> header file. */
+//#define HAVE_SYS_EPOLL_H 1
+
+/* Define to 1 if you have the <sys/event.h> header file. */
+/* #undef HAVE_SYS_EVENT_H */
+
+/* Define to 1 if you have the <sys/file.h> header file. */
+//#define HAVE_SYS_FILE_H 1
+
+/* Define to 1 if you have the <sys/inotify.h> header file. */
+//#define HAVE_SYS_INOTIFY_H 1
+
+/* Define to 1 if you have the <sys/mman.h> header file. */
+#define HAVE_SYS_MMAN_H 1
+
+/* Define to 1 if you have the <sys/mount.h> header file. */
+#define HAVE_SYS_MOUNT_H 1
+
+/* Define to 1 if you have the <sys/prctl.h> header file. */
+#define HAVE_SYS_PRCTL_H 1
+
+/* Define to 1 if you have the <sys/ptrace.h> header file. */
+#define HAVE_SYS_PTRACE_H 1
+
+/* Define to 1 if you have the <sys/select.h> header file. */
+#define HAVE_SYS_SELECT_H 1
+
+/* Define to 1 if you have the <sys/socket.h> header file. */
+#define HAVE_SYS_SOCKET_H 1
+
+/* Define to 1 if you have the <sys/statfs.h> header file. */
+#define HAVE_SYS_STATFS_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/syscall.h> header file. */
+#define HAVE_SYS_SYSCALL_H 1
+
+/* Define to 1 if you have the <sys/sysinfo.h> header file. */
+//#define HAVE_SYS_SYSINFO_H 1
+
+/* Define to 1 if you have the <sys/timex.h> header file. */
+//#define HAVE_SYS_TIMEX_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <sys/user.h> header file. */
+#define HAVE_SYS_USER_H 1
+
+/* Define to 1 if you have the <sys/utsname.h> header file. */
+#define HAVE_SYS_UTSNAME_H 1
+
+/* Define to 1 if you have the <sys/vfs.h> header file. */
+//#define HAVE_SYS_VFS_H 1
+
+/* Define to 1 if you have the `tanl' function. */
+#define HAVE_TANL 1
+
+/* Define to 1 if you have the `tee' function. */
+//#define HAVE_TEE 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the `unlinkat' function. */
+//#define HAVE_UNLINKAT 1
+
+/* Define to 1 if you have the `unsetenv' function. */
+#define HAVE_UNSETENV 1
+
+/* Define to 1 if you have the `unshare' function. */
+//#define HAVE_UNSHARE 1
+
+/* Define to 1 if you have the <ustat.h> header file and it works. */
+/* #undef HAVE_USTAT_H */
+
+/* Define to 1 if you have the `utimensat' function. */
+//#define HAVE_UTIMENSAT 1
+
+/* Define to 1 if you have the <utime.h> header file. */
+#define HAVE_UTIME_H 1
+
+/* Define to 1 if you have the `wait4' function. */
+#define HAVE_WAIT4 1
+
+/* Define if the linker support split stack adjustments */
+/* #undef LINKER_SUPPORTS_SPLIT_STACK */
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+/* #undef PACKAGE */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "package-unused"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "package-unused version-unused"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "libgo"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "version-unused"
+
+/* Define if setcontext clobbers TLS variables */
+/* #undef SETCONTEXT_CLOBBERS_TLS */
+
+/* The size of `void *', as computed by sizeof. */
+#define SIZEOF_VOID_P 8
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define if we're to use libffi. */
+#define USE_LIBFFI 1
+
+/* Define if the compiler supports -fsplit-stack */
+//#define USING_SPLIT_STACK 1
+
+/* Version number of package */
+/* #undef VERSION */
+
+/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
+   significant byte first (like Motorola and SPARC, unlike Intel). */
+#if defined AC_APPLE_UNIVERSAL_BUILD
+# if defined __BIG_ENDIAN__
+#  define WORDS_BIGENDIAN 1
+# endif
+#else
+# ifndef WORDS_BIGENDIAN
+/* #  undef WORDS_BIGENDIAN */
+# endif
+#endif
+
+/* Define to `long int' if <sys/types.h> does not define. */
+/* #undef off_t */
